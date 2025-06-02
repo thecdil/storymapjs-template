2d460ecb05df1f447b6fe620571092e647be64aa
Hoàng Phúc Trần <hoangphuctran@192.168.1.11.non-exists.ptr.local>
2025-06-02 13:54:57 +0700

    Set baseurl for GitHub Pages deployment

--- _site/assets/js/lunr-store.js
+++ _site/assets/js/lunr-store.js
@@ -2,42 +2,42 @@ var store = [
     
     
     { 
-        "url": "/embed.html",
+        "url": "/storymapjs-template/embed.html",
         "title": "Example StoryMap Embed",
         "text": ""
     },
     { 
-        "url": "/example.html",
+        "url": "/storymapjs-template/example.html",
         "title": "Example Page",
         "text": "Content can be written in Markdown above a StoryMap. This example StoryMap is added using an existing StoryMapJS format JSON file with media hosted externally, featuring items from University of Idaho Library’s William Allen Stonebraker Collection."
     },
     { 
-        "url": "/index.html",
+        "url": "/storymapjs-template/index.html",
         "title": "Khám phá Quảng Ngãi",
         "text": "giới thiệu thêm về trang chủ"
     },
     { 
-        "url": "/ly-son.html",
+        "url": "/storymapjs-template/ly-son.html",
         "title": "Khám phá Quảng Ngãi",
         "text": ""
     },
     { 
-        "url": "/sa-huynh.html",
+        "url": "/storymapjs-template/sa-huynh.html",
         "title": "Khám phá Quảng Ngãi",
         "text": ""
     },
     { 
-        "url": "/search.html",
+        "url": "/storymapjs-template/search.html",
         "title": "Site Search",
         "text": "Search site content using Lunr.js."
     },
     { 
-        "url": "/collections/",
+        "url": "/storymapjs-template/collections/",
         "title": "All Collections",
         "text": ""
     },
     { 
-        "url": "/c/",
+        "url": "/storymapjs-template/c/",
         "title": null,
         "text": ""
     }];
